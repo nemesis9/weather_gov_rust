2523a08ecdbfd09b17ec66e598cd5317db5ee4b8
Tony Brown <zyxavan@gmail.com>
2024-03-27 17:45:27 -0700

    initial attempt

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use log::{info, warn};
+
+// Project:        weather_gov
+// Description:    for given stations, get observations
+//
+// Outline:
+//     1.  Parse yaml config
+//     2.  Get station list from config
+//     3.  For each station, periodically get observations from weather.gov
+
+fn main() {
+    println!("Welcome to weather_gov");
+}
